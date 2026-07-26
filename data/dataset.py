e8fab72698083aecafe3b0b60b8b0e8ad29f6738
Pranjal Bainsla <pbainsla18082004@gmail.com>
2026-07-26 17:45:26 +0530

    fix: load input.txt from data directory instead of project root

--- data/dataset.py
+++ data/dataset.py
@@ -5,8 +5,8 @@ from config import (
     device,
 )
 # !wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('input.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
+with open('data/input.txt', 'r', encoding='utf-8') as f:
+   text = f.read()
 
 # here are all the unique characters that occur in this text
 chars = sorted(list(set(text)))
