fix: correct RoPE broadcasting dimensions

model/attention/mha.py
@@ -11,11 +11,11 @@ from config import (
 class Head(nn.Module):
     """ one head of self-attention """
 
-    def __init__(self, head_size):
+    def __init__(self, head_dim):
         super().__init__()
-        self.key = nn.Linear(n_embd, head_size, bias=False)
-        self.query = nn.Linear(n_embd, head_size, bias=False)
-        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.key = nn.Linear(n_embd, head_dim, bias=False)
+        self.query = nn.Linear(n_embd, head_dim, bias=False)
+        self.value = nn.Linear(n_embd, head_dim, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
         self.dropout = nn.Dropout(dropout)
 
@@ -23,7 +23,8 @@ class Head(nn.Module):
         B,T,C = x.shape
         k = self.key(x)   # (B,T,C)
         q = self.query(x) # (B,T,C)
-
+        cos = cos.unsqueeze(0) # (1, T, head_dim/2)
+        sin = sin.unsqueeze(0)
         q = apply_rope(q, cos, sin)
         k = apply_rope(k, cos, sin)
         # compute attention scores ("affinities")

model/attention/mha_optimized.py
@@ -41,8 +41,8 @@ class MultiHeadAttentionOptimized(nn.Module):
         V = V.transpose(1, 2)
 
         # apply RoPE
-        cos = cos[:T].unsqueeze(0).unsqueeze(0)  # (1, 1, T, D/2)
-        sin = sin[:T].unsqueeze(0).unsqueeze(0)  # (1, 1, T, D/2)
+        cos = cos.unsqueeze(0).unsqueeze(0)  # (1, 1, T, head_dim/2)
+        sin = sin.unsqueeze(0).unsqueeze(0)  # (1, 1, T, head_dim/2)
         Q = apply_rope(Q, cos, sin)
         K = apply_rope(K, cos, sin)
 

model/attention/rope.py
@@ -4,7 +4,7 @@ def precompute_freqs(head_size, max_seq_len, device):
     theta = 1.0 / (
         10000 ** (torch.arange(0, head_size, 2, device=device).float() / head_size)
     )
-    positions = torch.arange(max_seq_len, device=device)
+    positions = torch.arange(max_seq_len, device=device) # max_seq_len == block_size == T
     freqs = torch.outer(positions, theta)
     cos = freqs.cos()
     sin = freqs.sin()

model/gpt.py
@@ -26,8 +26,8 @@ class GPT(nn.Module):
 
     def forward(self, idx, targets=None):
         B, T = idx.shape
-        cos = self.cos[:T].unsqueeze(0)   # (1, T, n_embd/2)
-        sin = self.sin[:T].unsqueeze(0)
+        cos = self.cos[:T]  # (T, n_embd/2)
+        sin = self.sin[:T]
         # idx and targets are both (B,T) tensor of integers
         tok_emb = self.token_embedding_table(idx) # (B,T,C)
         # pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)