b9800fc5bcc97e983cb338afab25afdb9bdfdf68
Pranjal Bainsla <pbainsla18082004@gmail.com>
2026-09-22 23:57:44 +0530

    introduce nanogpt's configurator

---
+++ configs/base.py
@@ -0,0 +1,33 @@
+# Dataset
+dataset_path = "data/input.txt"
+
+# training
+batch_size = 16
+block_size = 32
+max_iters = 5000
+eval_interval = 100
+learning_rate = 1e-3
+eval_iters = 200
+
+# model
+n_embd = 64
+n_head = 4
+n_layer = 4
+num_experts = 4
+num_shared_experts = 2 # DeepSeek V3 uses 1
+gamma = 0.001 # bias update speed 
+k = 2
+dropout = 0.0
+alpha = 0.001
+n_kv_heads = 2
+latent_kv_dim = 32 # dc -> KV compression dimension
+latent_q_dim = 32 # dc' -> Query compression dimension
+
+# inference
+max_seq_len = 4096
+
+# Architecture
+attention = "mla_deepseek_optimized"      # mha, gqa, mla, mla_deepseek_optimized
+ffn = "moe_deepseek"                      # mlp, moe, swiglu
+norm = "rmsnorm"                          # layernorm, rmsnorm
+position = "rope"                         # rope, learned, alibi
